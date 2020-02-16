5c166295b161b78050c78a41af6d28b915498303
Lemuel Reyes <reyeslemuel@hotmail.com>
2020-02-16 01:04:57 -0800

    exercise 18. Use hasAttribute method.

---
+++ browserapi/script18.js
@@ -0,0 +1,25 @@
+// # Exercise 18
+
+// * Create the following folder/file structure:
+// ```
+// /ex_18
+//   |-- index.html
+// ```
+
+// ## index.html
+// * Create a basic HTML document
+// * Create a script tag on the document head element
+// * Add the following html code
+
+
+// * Select the first link element
+// * Add the blue class if the element has the class attribute
+// * If not, then add the red class
+
+const firstLink = document.querySelector('a')
+
+if(firstLink.hasAttribute('class')) {
+    firstLink.classList.add('azul')
+} else {
+    firstLink.classList.add('rojo')
+}
