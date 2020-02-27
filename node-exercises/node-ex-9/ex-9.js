6918cbb07013284bc12ac1699544ee622ac1ddd3
Lemuel Reyes <reyeslemuel@hotmail.com>
2020-02-26 18:07:50 -0800

    create a server using express, use loggerplease, req, res

---
+++ node-exercises/node-ex-9/ex-9.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const Logger = require('logplease');
+const logger = Logger.create('utils');
+
+app.get('/', function(request, response){
+    response.send('Congrats youre using your first Node.js and Express as Web Server');
+}); 
+
+app.listen(3000, (err) => {
+
+    if(err) {
+        logger.error('Uhoh! something went wrong')
+    }
+        logger.info(`This HTTP server is running on port ${3000}
+    `)
+});
